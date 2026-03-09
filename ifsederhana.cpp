6759543424281d40a81c4e5dbbd9c98706785bbc
canzonadzka <canzonadzka@gmail.com>
2026-03-08 21:21:14 -0700

    menampilkan hasil nilai x ke layar

--- ifsederhana.cpp
+++ ifsederhana.cpp
@@ -13,5 +13,8 @@ int main()
     if (x == 5)
     {
         x = x + 1;
+        cout << "nilai x sudah dikoreksi menjadi : " << x << endl;
     }
+
+    cout << "Bilangan yang diberikan = " << x;
 }
